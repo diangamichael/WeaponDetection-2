8e201653a8a9e790a52a7c1ca44ccc4d153b84cc
Aidan Farhi <aidanmcfarhi@gmail.com>
2021-09-07 18:33:23 -0400

    getting links script working

---
+++ DataPipeline/pipeline.py
@@ -0,0 +1,20 @@
+
+
+def get_urls_from_google():
+    pass
+
+
+def dump_urls_to_s3():
+    pass
+
+
+def download_images():
+    pass
+
+
+def remove_duplicate_images():
+    pass
+
+
+def dump_images_to_s3():
+    pass
