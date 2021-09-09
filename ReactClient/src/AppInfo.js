13862eac98948c91c010a60bd2e3e153590a70c5
Aidan Farhi <aidanmcfarhi@gmail.com>
2021-09-09 12:49:54 -0400

    added to info page

--- ReactClient/src/AppInfo.js
+++ ReactClient/src/AppInfo.js
@@ -1,8 +1,14 @@
 
 export default function AppInfo() {
     return (
-        <div>
-            App info page
+        <div id='info-main-div'>
+            <h1>How does it work?</h1>
+            <hr></hr>
+            <p>1. The application sends your image to a server (a computer somewhere).</p>
+            <p>2. The server feeds the image to an Object Detection Model (Machine Learning).</p>
+            <p>3. Once the image has been proccessed by the Model, it sends the result image to cloud storage (another computer somewhere).</p>
+            <p>4. Once the application receives confirmation that the result is ready, it grabs that image from cloud Storage and
+                  displays the result.</p>
         </div>
     )
 }
