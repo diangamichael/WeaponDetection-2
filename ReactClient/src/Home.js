81a5c7d396c8a94b0e0ebf30e72709d9e8b9cc78
Aidan Farhi <aidanmcfarhi@gmail.com>
2021-09-09 12:39:53 -0400

    reformatted code

--- ReactClient/src/Home.js
+++ ReactClient/src/Home.js
@@ -6,35 +6,35 @@ import Spinner from 'react-bootstrap/Spinner'
 export default function Home() {
 
     // state objects
-  const [file, setFile] = useState(null)
-  const [imageId, setImageId] = useState('')
-  const [renderImage, setRenderImage] = useState(false)
-  const [loading, setLoading] = useState(false)
+    const [file, setFile] = useState(null)
+    const [imageId, setImageId] = useState('')
+    const [renderImage, setRenderImage] = useState(false)
+    const [loading, setLoading] = useState(false)
 
-  // sets file in state
-  const handleChange = event => {
-    setRenderImage(false)
-    setFile(event.target.files[0])
-  }
+    // sets file in state
+    const handleChange = event => {
+        setRenderImage(false)
+        setFile(event.target.files[0])
+    }
 
-  // submits file to api
-  const handleSubmit = async() => {
-    // make sure file was received by input
-    if (file === undefined || file === null) return
-    setRenderImage(false)
-    setLoading(true)
-    const formData = new FormData()
-    formData.append("imageUpload", file, file.name)
-    try {
-      const res = await axios.post('https://flask-service.e0db4cmami9tu.us-east-1.cs.amazonlightsail.com/upload_file', formData)
-      if (res.data.result === 'success') {
-        // response will include id of result image to pull from s3 bucket
-        setImageId(res.data.imageId)
-        setRenderImage(true)
-        setLoading(false)
-      }
-    } catch(er) { console.log(er)}
-  }
+    // submits file to api
+    const handleSubmit = async() => {
+        // make sure file was received by input
+        if (file === undefined || file === null) return
+        setRenderImage(false)
+        setLoading(true)
+        const formData = new FormData()
+        formData.append("imageUpload", file, file.name)
+        try {
+        const res = await axios.post('https://flask-service.e0db4cmami9tu.us-east-1.cs.amazonlightsail.com/upload_file', formData)
+        if (res.data.result === 'success') {
+            // response will include id of result image to pull from s3 bucket
+            setImageId(res.data.imageId)
+            setRenderImage(true)
+            setLoading(false)
+        }
+        } catch(er) { console.log(er)}
+    }
 
     return(
         <div>
